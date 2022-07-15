76ac86346de67fd7fdbe3ea11d5ad91e6b1bd6c4
Teja Madduri <teecha23@gmail.com>
2022-07-15 13:52:20 -0400

    Added popup to markers

--- src/components/CRUD/index.js
+++ src/components/CRUD/index.js
@@ -1,21 +1,22 @@
 import React from "react";
 import StartFirebase from "../FirebaseConfig";
 import { ref, onValue } from "firebase/database";
-import Map, {Marker,Popup} from 'react-map-gl';
+import Map, {GeolocateControl, Marker,Popup} from 'react-map-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
-import MapPopup from "./mapPopup";
+import { positions } from "@mui/system";
 
 
 const firedb = StartFirebase();
 const dbref = ref(firedb,"EWD");
-
+var ini_lat = 33.305241
+var ini_lon = -111.680139
 export class Realtimedata extends React.Component{
 
 constructor(){
     super();
     this.state = {
         mydata: [],
-        showPopup : false
+        showPopup : true
     }
 }
 
@@ -34,14 +35,15 @@ constructor(){
     render(){
         return(
             <div>
+                
                 <table>
                     <tbody>
                     <tr>
                 <Map
                     initialViewState={{
-                        latitude: 33.305241,
-                        longitude: -111.680139,
-                        zoom: 5
+                        latitude: ini_lat,
+                        longitude: ini_lon,
+                        zoom: 3
                     }}
                     style={{width: '100vw', height: '77vh'}}
                     mapStyle="mapbox://styles/mapbox/streets-v9"
@@ -53,7 +55,23 @@ constructor(){
                     {console.log(test.data.Latitude)}
                     {console.log(test.data.Longitude)}
                     {console.log("----------------------")}
-                    <Marker longitude={test.data.Longitude} latitude={test.data.Latitude} color="slateblue"/>
+                    <Marker longitude={test.data.Longitude} latitude={test.data.Latitude} color="green"/>
+                    {this.state.showPopup && (
+                            <Popup longitude={test.data.Longitude} latitude={test.data.Latitude}
+                                anchor="bottom"
+                                offset={25}
+                                onClose={() => this.setState({showPopup : false})}>
+                                {this.state.mydata.map((row,index) => 
+                                            <td>
+                                                <th><h4>{row.key}</h4></th>
+                                                <tr>EKG : {row.data.EKG}</tr>
+                                                <tr>SPO2 : {row.data.Spo2}</tr>
+                                                <tr>Temp : {row.data.Temprature}</tr>
+                                                <tr>Lat : {row.data.Latitude}</tr>
+                                                <tr>Lon : {row.data.Longitude}</tr>
+                                            </td>
+                                )}
+                            </Popup>)}
                     </div>)}
                 </Map>
                 </tr>
